bb08c134b90e74c61c31ac9d56ab25f46eaea33e
AndersonHqds <andyhhenrikefs@gmail.com>
2019-01-21 22:46:03 -0200

    Software de distribuição binomial

---
+++ src/Estatistica.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.text.DecimalFormat;
+import javax.swing.ImageIcon;
+
+
+
+public class Estatistica {
+    
+
+
+    public static void main(String[] args) 
+    {
+        Interface p = new Interface();
+        p.setVisible(true);   
+        
+    }
+    
+
+}
